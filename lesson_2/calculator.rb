72f523fb2497b1021ba253ca9b9533fdc410957e
Rui Pires <ruiapires@outlook.pt>
2016-10-13 18:48:25 +0100

    Run Rubocop against calculator.rb file

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -8,7 +8,7 @@ def prompt(message)
 end
 
 def valid_number?(num)
-  num.to_i != 0
+  num.to_i.nonzero?
 end
 
 def operation_to_message(op)
@@ -87,15 +87,15 @@ loop do # Main loop
   prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
-            when '1'
-              number1.to_i + number2.to_i
-            when '2'
-              number1.to_i - number2.to_i
-            when '3'
-              number1.to_i * number2.to_i
-            when '4'
-              number1.to_f / number2.to_f
-  end
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
 
   prompt("The result is #{result}")
 
