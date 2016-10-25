9ac47e4a966f409988b95224123aaab915df7a00
Rui Pires <ruiapires@outlook.pt>
2016-10-25 18:00:24 +0100

    Refactor car_loan_calculator.rb

--- lesson_2/car_loan_calculator.rb
+++ lesson_2/car_loan_calculator.rb
@@ -1,114 +1,79 @@
-require 'yaml'
-
-MESSAGES = YAML.load_file('car_loan_calculator.yml')
-
 def prompt(message)
   puts "=> #{message}"
 end
 
-def integer?(input)
-  input.to_i.to_s == input
-end
-
-def float?(input)
-  input.to_f.to_s == input
-end
-
-def valid_number?(input)
-  integer?(input) || float?(input)
-end
-
-def to_i_or_to_f?(number)
-  if integer?(number)
-    number.to_i
-  else
-    number.to_f
+def valid_numbers(input, prompt)
+  input = nil
+  loop do
+    input = gets.chomp
+    if /^\d+$/.match(input) && input.to_i > 0
+      break
+    else
+      prompt("Please use only numbers!")
+    end
   end
+  input
 end
 
-# Welcome to the calculator and ask for a valid name
-prompt(MESSAGES['welcome'])
+def valid_interest(input, prompt)
+  input = nil
+  loop do
+    input = gets.chomp
+    if input.to_f <= 0
+      prompt("Please use only numbers bigger than 0!")
+    elsif /^\w+^\$/.match(input)
+      prompt("Please use only numbers!")
+    elsif input.size > 4
+      prompt("Please insert a valid interest rate!")
+    else
+      break
+    end
+  end
+  input
+end
 
 name = ''
+prompt("Welcome to the Car Loan Calculator! Enter your name please: ")
 loop do
-  name = gets.chomp
-
+  name = gets.chomp.capitalize
   if name.empty?
-    prompt(MESSAGES['valid_name'])
+    prompt("Please insert a name!")
+  elsif name.size <= 1 || name.size > 10
+    prompt("Please use 2 up to 10 characters for your name!")
+  elsif /\d+/.match(name) || /\W+/.match(name)
+    prompt("Your name can't have numbers or special characters!")
   else
     break
   end
 end
 
-# ask for:
-# the loan amount
-loop do # Main Loop
-  amount = ''
-  loop do
-    prompt("#{MESSAGES['amount']} #{name}?")
-    amount = gets.chomp
+loop do
+  amount = valid_numbers(amount, prompt("What's the loan
+    amount that you need? Please type a number - ex: 12500,
+    not 12,500 or 12.500"))
+  amount = amount.to_i
 
-    if valid_number?(amount)
-      break
-    else
-      prompt(MESSAGES['valid_number'])
-    end
-  end
+  annual_percentage_rate = valid_interest(annual_percentage_rate,
+    prompt("What's the Annual Percentage Rate that you wish?
+            Example: Input 5 for 5% rate"))
+  annual_percentage_rate = annual_percentage_rate.to_f
 
-  amount = to_i_or_to_f?(amount)
+  loan_duration = valid_numbers(loan_duration,
+    prompt("How many years is the duration of the loan?
+            Please type in years."))
+  loan_duration = loan_duration.to_i
 
-  # the Annual Percentage Rate
-  rate = ''
-  loop do
-    prompt(MESSAGES['rate'])
-    rate = gets.chomp.to_f
+  monthly_interest = annual_percentage_rate / 100 / 12
+  duration_in_months = loan_duration * 12
 
-    if rate > 1
-      break
-    else
-      prompt(MESSAGES['valid_number'])
-    end
-  end
+  monthly_payment = amount * (monthly_interest / (1 -
+    (1 + monthly_interest)**-duration_in_months))
 
-  rate = to_i_or_to_f?(rate)
+  prompt("#{name} your monthly payment is
+    $#{format('%02.2f', monthly_payment)}")
 
-  # the loan duration
-  duration = ''
-  loop do
-    prompt(MESSAGES['duration'])
-    duration = gets.chomp
-
-    if valid_number?(duration)
-      break
-    else
-      prompt(MESSAGES['valid_number'])
-    end
-  end
-
-  duration = to_i_or_to_f?(duration)
-
-  # Calculate:
-  # monthly interest rate
-  # loan duration in months
-
-  monthly_interest = (rate * 0.010) / 12
-  loan_duration = duration * 12
-  without_interest = amount / loan_duration
-
-  result = amount * (monthly_interest /
-    (1 - (1 + monthly_interest)** -loan_duration))
-
-  interests = result - without_interest
-  total = result * loan_duration
-
-  prompt("#{MESSAGES['result']} #{result.round(2)}")
-  prompt("#{MESSAGES['result2']} #{without_interest}
-    #{MESSAGES['result3']} #{interests.round(2)}.
-    #{MESSAGES['result4']} #{total.round(2)}")
-
-  prompt(MESSAGES['continue'])
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  prompt("Would you like to calculate again?
+    'Y' to continue or any other key to exit")
+  answer = gets.chomp.downcase.to_s
+  break unless answer == 'y'
 end
-
-prompt("#{MESSAGES['finish']} #{name}!")
