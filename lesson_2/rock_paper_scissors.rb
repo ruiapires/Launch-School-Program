8678e8d857728ca7cebd316fef38c5493d8b6152
Rui Pires <ruiapires@outlook.pt>
2016-10-27 14:59:29 +0100

    Update rock_paper_scissors.rb file

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -4,18 +4,27 @@ def prompt(message)
   puts ">>>>>> #{message}"
 end
 
-def transform(input, one_letter)
-  if input == 'r'
-    one_letter << VALID_CHOICES[0]
-  elsif input == 'p'
-    one_letter << VALID_CHOICES[1]
-  elsif input == 's'
-    one_letter << VALID_CHOICES[2]
-  elsif input == 'v'
-    one_letter << VALID_CHOICES[3]
-  elsif input == 'l'
-    one_letter << VALID_CHOICES[4]
+def letter_s(input, var)
+  if input == 's'
+    var << VALID_CHOICES[2]
+  elsif input == 'S'
+    var << VALID_CHOICES[3]
   end
+end
+
+def other_letters(input, var)
+  if input == 'r'
+    var << VALID_CHOICES[0]
+  elsif input == 'p'
+    var << VALID_CHOICES[1]
+  elsif input == 'l'
+    var << VALID_CHOICES[4]
+  end
+end
+
+def transform(input, one_letter)
+  (letter_s(input, one_letter) ||
+    other_letters(input, one_letter))
 end
 
 def rock(win, lose)
@@ -52,33 +61,58 @@ end
 def display_results(player, computer)
   if win?(player, computer)
     prompt("You won!")
-  elsif   win?(computer, player)
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
   end
 end
 
-loop do
-  choice = ''
-  single_letter = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp.downcase.to_s
-
-    if transform(choice, single_letter)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+def final_result(player, computer)
+  if player == 5
+    prompt("Congratulations! You have won the game!")
+  elsif computer == 5
+    prompt("You lost the game! Better luck next time!")
   end
+end
 
-  computer_choice = VALID_CHOICES.sample
+loop do
+  player_wins = 0
+  computer_wins = 0
+  loop do
+    choice = ''
+    single_letter = ''
+    loop do
+      prompt("Welcome to #{VALID_CHOICES.join(', ')} game.
+        Please type:
+        'r': rock, 'p': paper, 's': scissors,'S': spock, 'l': lizard")
 
-  prompt("You chose: #{single_letter}; Computer chose: #{computer_choice}")
+      choice = gets.chomp
 
-  display_results(single_letter, computer_choice)
+      if transform(choice, single_letter)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
+    end
 
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose: #{single_letter}; Computer chose: #{computer_choice}")
+
+    display_results(single_letter, computer_choice)
+
+    if win?(single_letter, computer_choice)
+      player_wins += 1
+    elsif win?(computer_choice, single_letter)
+      computer_wins += 1
+    end
+
+    prompt("The result is: You #{player_wins} Computer #{computer_wins}")
+
+    final_result(player_wins, computer_wins)
+    break if player_wins == 5 || computer_wins == 5
+  end
   prompt("Do you want to play again? 'Y' to continue or any key to exit")
   answer = gets.chomp
   break unless answer.casecmp('y').zero?
